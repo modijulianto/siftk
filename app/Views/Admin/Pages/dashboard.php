cdfd5613418239285b7cc8e401f12a90631b5072
modijulianto <putumodi25@gmail.com>
2021-05-21 12:49:49 +0800

    dashboard

--- app/Views/Admin/Pages/dashboard.php
+++ app/Views/Admin/Pages/dashboard.php
@@ -1,5 +1,12 @@
 <?= $this->extend('Admin/layout'); ?>
 
 <?= $this->section('content'); ?>
-awdkaowkd
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="tab-content-details shadow-reset">
+            <img src="/img/logo-ftk.png" alt="Logo FTK" width="150px">
+            <h2 style="margin-top: 20px;">Selamat datang di Sistem Informasi Fakultas Pendidikan Ganesha</h2>
+        </div>
+    </div>
+</div>
 <?= $this->endSection(); ?>
